3c78170e59babc89f7921fd161bc338ab836b8ea
Nathalia Cristina <nathaliacristina20@hotmail.com>
2025-02-16 18:13:57 -0300

    feature: avatar component

---
+++ src/app/components/avatar/avatar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { IconComponent } from '../icon/icon.component';
+
+@Component({
+  selector: 'app-avatar',
+  imports: [IconComponent],
+  templateUrl: './avatar.component.html',
+  styleUrl: './avatar.component.scss'
+})
+export class AvatarComponent {
+
+}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { BreadcrumbComponent } from "../breadcrumb/breadcrumb.component";
+import { IconComponent } from '../icon/icon.component';
+import { ButtonComponent, ButtonSize, ButtonStyle } from '../button/button.component';
+import { AvatarComponent } from '../avatar/avatar.component';
 
 @Component({
   selector: 'app-header',
-  imports: [BreadcrumbComponent],
+  imports: [BreadcrumbComponent, IconComponent, ButtonComponent, AvatarComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  public buttonSizeEnum = ButtonSize;
+  public buttonStyleEnum = ButtonStyle;
 }
